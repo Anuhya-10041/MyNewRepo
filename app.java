fb0c7efdd8c5c7a7089fd9a4a7682fcde3d3ced9
Anuhya-10041 <arramanuhyareddy@gmail.com>
2025-11-08 19:14:47 +0530

    Added 2nd code

--- app.java
+++ app.java
@@ -3,3 +3,13 @@ public static void main(String[] args) {
 System.out.println("Hello World");
 }
 }
+//dev 2 push from local
+
+public class AddTwoIntegers {
+   public static void main(String[] args) {
+       int first = 10;
+       int second = 20;
+       int sum = first + second;
+       System.out.println("The sum is: " + sum);
+   }
+}
